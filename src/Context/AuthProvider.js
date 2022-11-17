30c868b5bda689734825eb9e6997383a461b1517
sajib689 <jibonraja11@gmail.com>
2022-11-17 13:17:20 +0600

    second commit

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -8,21 +8,26 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
     const createUser = (email,password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
     const signIn = (email,password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
     const updateUser = (userInfo) => {
         return updateProfile(user, userInfo)
     }
     const logOut = () => {
+        setLoading(true)
         return signOut(auth)
     }
     useEffect(() => {
       const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false)
         })
+
 
         return () => unSubscribe();
     },[])
@@ -32,7 +37,8 @@ const AuthProvider = ({children}) => {
         signIn,
         updateUser,
         logOut,
-        user
+        user,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
